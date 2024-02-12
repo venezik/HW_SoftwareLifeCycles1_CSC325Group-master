46268aaa1d1bc0da4a9dbc36ca7068fc09f5dd62
EmeAkpan <53278562+EmeAkpan@users.noreply.github.com>
2024-02-12 12:36:19 -0500

    All ToDo's On Intern and Staff class done

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -2,24 +2,24 @@ package com.mycompany.agency;
 
 
 
-    // TODO 1: Make Intern a child of StaffEmploee
+    // TODO 1: Make Intern a child of StaffEmployee (Task Done)
 
-public class Intern 
+public class Intern extends StaffEmployee
 {
     //-----------------------------------------------------------------
-    //  TODO 2: Constructor: Sets up this intern using the specified
+    //  TODO 2: Constructor: Sets up this intern using the specified (Task Done)
     //  information.
     //-----------------------------------------------------------------
     public Intern(String eName, String eAddress, String ePhone)
     {
-        
+        super(eName, eAddress, ePhone);
     }
 
     //-----------------------------------------------------------------
-    // TODO 3:  Returns a zero pay value for this intern.
+    // TODO 3:  Returns a zero pay value for this intern. (Task Done)
     //-----------------------------------------------------------------
     public double pay()
     {
-      
+        return 0.0;
     }
 }
--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -14,7 +14,14 @@ public class Staff
     {
         staffList = new StaffMember[6];
 
-        // TODO 1: Add 6 emploees to the staffList
+        // TODO 1: Add 6 employees to the staffList (Task Done)
+
+        staffList[0] = new Executive("John Smith", "123 Main St", "555-1234", "123-45-6789", 100000.0);
+        staffList[1] = new StaffEmployee("Angelina Jolie", "987 Cedar St", "555-0987", "516-333476", 89.00);
+        staffList[2] = new StaffEmployee("Pharell Williams", "789 Elm St", "555-7890","329-964785",89.0);
+        staffList[3] = new TempEmploee("Aubrey Graham", "321 Pine St", "555-4321","992-759032", 40.0);
+        staffList[4] = new Intern("Salma Hayek", "654 Birch St", "555-8765");
+        staffList[5] = new Intern("George Washington", "456 Oak St", "555-5678");
 
         ((Executive)staffList[0]).awardBonus(500.00);
 
